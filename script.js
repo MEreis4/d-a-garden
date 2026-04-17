dd08eaa93780b6468d6e5d5e9619c441c68b2759
Murilo <muriloreis18769@gmail.com>
2026-04-17 19:18:01 -0300

    Refactor planet rotation logic and enhance keyboard controls for speed adjustment

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Animação de rotação da pequena planeta
 const littlePlanet = document.querySelector('.little-planet');
 let start;
-function rotatePlanet(timestamp) {
+let speedLP = 0.2; // Velocidade de rotação do Little Planet
+let angleLP = 0; // angulo inicial do Little Planet
+
+function rotatePlanet() {
     if (!littlePlanet) return;
 
-    if (!start) start = timestamp;
-    const elapsed = timestamp - start;
-
-    const angleLP = elapsed * 0.01; // Ajuste a velocidade de rotação aqui
+    angleLP += speedLP; // Ajuste a velocidade de rotação aqui
     littlePlanet.style.transform = `rotate(${angleLP}deg)`;
     requestAnimationFrame(rotatePlanet);
 }
@@ -16,7 +16,7 @@ rotatePlanet();
 // Animação de órbita do OVNI
 const radiusX= 250;
 const radiusY = 60;
-const speed = 0.001;
+const speed = 0.0004;
 const ovni = document.querySelector('.ovni');
 
 function orbitingAnimation(timestamp) {
@@ -37,12 +37,41 @@ function orbitingAnimation(timestamp) {
         ovni.style.zIndex = 2; // OVNI à frente do planeta
     }
     requestAnimationFrame(orbitingAnimation);
-
-    //manipulação da velocidade
-    document.addEventListener('keypress', (event)=>{
-        if (event.key === 'ArrowLeft'){
-            console.log(event.key)
-        }
-    })
 }
 orbitingAnimation();
+
+    //manipulação a rotação do planeta com as setas do teclado
+    function adjustPlanetRotation() {
+    let speedLpLimitor = 20 || -20; // Limite para a velocidade de rotação
+    document.addEventListener('keydown', (event)=>{
+        if (event.key === 'ArrowLeft'){
+            speedLP -= 0.1;
+            console.log('Girando para esquerda');
+            if (speedLP < -speedLpLimitor) {
+                speedLP = -speedLpLimitor; // Limita a velocidade máxima para a esquerda
+                console.log('Velocidade máxima para a esquerda atingida');
+            }     
+        } else if (event.key === 'ArrowRight'){
+            speedLP += 0.1;
+            console.log('Girando para direita');
+            if (speedLP > speedLpLimitor) {
+                speedLP = speedLpLimitor; // Limita a velocidade máxima para a direita
+                console.log('Velocidade máxima para a direita atingida');
+            }
+    }});
+    }
+
+    document.addEventListener('keyup', (event)=>{
+        if (event.key === 'ArrowLeft'){
+            do {
+                speedLP += 0.1;
+            }
+                while (speedLP<0.2);
+        } else if (event.key === 'ArrowRight'){
+            speedLP -= 0.1;
+           do { speedLP -= 0.1;
+            } while (speedLP> 0.2) 
+
+    }});
+    
+adjustPlanetRotation();
